9d2851b5246f53d331e2dfddf3ed742a3dfc8918
LEGRAND Dylan (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / UO TRACTION DL ALSACE) <9919157t@commun.ad.sncf.fr>
2022-08-24 15:51:11 +0200

    solution quete 6

--- database.js
+++ database.js
@@ -10,23 +10,4 @@ const database = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
-// database
-//   .getConnection()
-//   .then(() => {
-//     console.log("Can reach database");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
-// database
-//   .query("select * from movies")
-//   .then((result) => {
-//     const movies = result[0];
-//     console.log(movies);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
   module.exports = database;
--- userHandlers.js
+++ userHandlers.js
@@ -1,16 +1,34 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
-  database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving data from database");
-    });
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+    
+    if (req.query.city != null) {
+      sql += " and city = ?";
+      sqlValues.push(req.query.city);
+    }
+  }
+  else if (req.query.city != null) {
+  sql += " where city = ?";
+  sqlValues.push(req.query.city);
+}
+database
+.query(sql, sqlValues)
+.then(([users]) => {
+  res.json(users);
+})
+.catch((err) => {
+  console.error(err);
+  res.status(500).send("Error retrieving data from database");
+});
 };
+
+
 
 const getUsersById = (req, res) => {
     const id = parseInt(req.params.id);

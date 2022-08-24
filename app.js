06f7a7a8950fa6872def77716d6e5650fdc46632
LEGRAND Dylan (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / UO TRACTION DL ALSACE) <9919157t@commun.ad.sncf.fr>
2022-08-24 09:21:09 +0200

    solution quete 4

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.get("/api/users/:id", userHandlers.getUsersById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("api/users", userHandlers.postUser);
 
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/users/:id", userHandlers.updateUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
